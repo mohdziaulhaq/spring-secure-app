379dc3b66a216bdaa385d3626b52232af2668d4d
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-03-13 04:50:53 -0500

    create spring secure app

---
+++ src/main/java/com/zia/security/SpringSecureAppApplication.java
@@ -0,0 +1,13 @@
+package com.zia.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecureAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecureAppApplication.class, args);
+	}
+
+}
