58bbe796a3bd208735d09180ceb19332284a9908
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-03-13 07:52:15 -0500

    updated route1 to show user name using Principal

--- src/main/java/com/zia/security/controllers/ApiController.java
+++ src/main/java/com/zia/security/controllers/ApiController.java
@@ -4,14 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
     //first end point
     @GetMapping("/route1")
-    public String route1(){
-        return "This is protected route1";
+    public String route1(Principal principal) {
+        return "This is protected route1"+principal.getName();
     }
 
     //second end point
